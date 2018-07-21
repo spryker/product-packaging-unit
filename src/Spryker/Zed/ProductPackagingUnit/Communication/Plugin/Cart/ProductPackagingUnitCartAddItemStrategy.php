d27f4e7599f64e57202021068bcc4697e8cb3404
Mahmoud Ali <mahmoud.ali@spryker.com>
2018-07-22 01:30:33 +0200

    PS-2992: ProductPackagingUnitCartAddItemStrategy and ProductPackagingUnitCartRemoveItemStrategy

---
+++ src/Spryker/Zed/ProductPackagingUnit/Communication/Plugin/Cart/ProductPackagingUnitCartAddItemStrategy.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\ProductPackagingUnit\Communication\Plugin\Cart;
+
+use Generated\Shared\Transfer\ItemTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Zed\CartExtension\Dependency\Plugin\CartItemOperationStrategyInterface;
+
+class ProductPackagingUnitCartAddItemStrategy extends ProductPackagingUnitAbstractCartItemOperationStrategy implements CartItemOperationStrategyInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\ItemTransfer $itemTransfer
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function excute(ItemTransfer $itemTransfer, QuoteTransfer $quoteTransfer): QuoteTransfer
+    {
+        foreach ($quoteTransfer->getItems() as $currentItemTransfer) {
+            if ($this->getItemIdentifier($currentItemTransfer) === $this->getItemIdentifier($itemTransfer)) {
+                $currentItemTransfer->setQuantity(
+                    $currentItemTransfer->getQuantity() + $itemTransfer->getQuantity()
+                );
+
+                $currentItemTransfer->setAmount(
+                    $currentItemTransfer->getAmount() + $itemTransfer->getAmount()
+                );
+
+                return $quoteTransfer;
+            }
+        }
+
+        $quoteTransfer->getItems()->append($itemTransfer);
+
+        return $quoteTransfer;
+    }
+}
